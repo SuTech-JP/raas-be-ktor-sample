f780466cdda41af6e4efecb348efa0788ceb5456
sunao@sutech.co.jp <sunao@sutech.co.jp>
2025-05-09 19:42:42 +0900

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,12 @@ plugins {
 group = "com.example"
 version = "0.0.1"
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
 application {
     mainClass.set("com.example.ApplicationKt")
 
@@ -40,7 +46,7 @@ dependencies {
     testImplementation("io.ktor:ktor-server-test-host-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
     implementation("io.ktor:ktor-server-cors:$ktor_version")
-    implementation("jp.co.sutech:raas-client-ktor:2.0.0")
+    implementation("jp.co.sutech:raas-client-ktor:2.1.0")
     implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
 
 }
